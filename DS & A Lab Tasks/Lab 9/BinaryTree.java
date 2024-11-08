618da3060cbd3516f40bd4675fe6e78c4d70b2ff
M-Adil-J <adilxjumani@gmail.com>
2024-11-08 11:42:24 +0500

    All Tasks are Added

---
+++ DS & A Lab Tasks/Lab 9/BinaryTree.java
@@ -0,0 +1,36 @@
+class BinaryTree {
+    // Root of Binary Tree
+    Node root;
+
+    // Constructors
+    BinaryTree(int key) {
+        root = new Node(key);
+    }
+
+    BinaryTree() {
+        root = null;
+    } 
+
+    public void addNode(int data) {
+        // insert elements in a tree so that left subtree of parent should contain smaller values
+        // and right sub-tree should contain larger than its parent.
+        // handle all possible exceptions/errors
+    } 
+
+    public boolean searchNode(int data) {
+        // search data from Binary Tree and return true/false, check all possible conditions
+        // handle all possible exceptions/errors
+    } 
+
+    public int countOfNodes(Node root) {
+        // return the number of nodes in a tree
+    }
+
+    public int height(Node root) {
+        // return the height/depth of the tree
+    } 
+
+    public static void main(String[] args) {
+        // Test the main method by creating node for different multiple nodes with children
+    }
+}
